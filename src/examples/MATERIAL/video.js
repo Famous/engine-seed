dfa77b74d91c9c272d2fc2f3dc86e17fe12fd8e9
Adnan Wahab <mail@adnanwahab.com>
2015-03-17 14:41:53 -0500

    make video use video and add comments

--- src/examples/MATERIAL/video.js
+++ src/examples/MATERIAL/video.js
@@ -10,20 +10,29 @@ var Origin = require('famous-components').Origin;
 var material = require('famous-webgl-materials').Material;
 
 var video = document.createElement('video');
-video.src = 'small.mp4';
+video.src = 'small.mp4'; //needs to be replaced with your own url
 video.load();
 video.play();
+video.muted = true;
+
+var t = material.Texture(video);
+
+var m = material.add([t, material.uv()]);
 
 function Ball(node) {
     this.dispatch = node.getDispatch();
 
     this.mesh = new Mesh(this.dispatch);
     this.mesh.setGeometry(new Sphere);
-    this.mesh.setBaseColor(material.Texture('images/famous_logo.png'));
+
+    this.mesh.setBaseColor(m);
 
     this.size = new Size(this.dispatch);
     this.rotation = new Rotation(this.dispatch);
+    this.position = new Position(this.dispatch);
+    
     this.size.setAbsolute(500, 500, 500);
+    this.position.set(200, 200);
 };
 
 module.exports = Ball;
