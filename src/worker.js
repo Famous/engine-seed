6dc1264557bfde65c72808547eec9a95cd0e3790
Ben Rowles <rowlesben@gmail.com>
2015-03-18 19:07:55 -0700

    updates lights, gesture, scroll demos

--- src/worker.js
+++ src/worker.js
@@ -1,4 +1,4 @@
-module.exports = require('./examples/gestures-demo/index');
+// module.exports = require('./examples/gestures-demo/index');
 // module.exports = require('./examples/textureSharing/index');
 // module.exports = require('./examples/helloWorld/index');
 // module.exports = require('./examples/MATERIAL/index');
@@ -7,7 +7,7 @@ module.exports = require('./examples/gestures-demo/index');
 // module.exports = require('./examples/eventing/index');
 // module.exports = require('./examples/twitter/index');
 // module.exports = require('./examples/origin/index');
-// module.exports = require('./examples/LightsGL/index');
+module.exports = require('./examples/LightsGL/index');
 // module.exports = require('./examples/solarSystem/index');
 // module.exports = require('./examples/noapi/index');
 // module.exports = require('./examples/physics-scroll/index');
