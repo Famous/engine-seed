d949e26aac4d8dbc89f1cd0f1ffeb931694ca347
Farhad Ghayour <f@famo.us>
2015-03-18 17:11:10 -0700

    Clean: Add plane to solar system

---
+++ src/examples/solarSystem/Plane.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/**
+ * Module dependencies
+ */
+var Origin = require('famous-components').Origin;
+var Rotation = require('famous-components').Rotation;
+var Position = require('famous-components').Position;
+var Align = require('famous-components').Align;
+var Origin = require('famous-components').Origin;
+var MountPoint = require('famous-components').MountPoint;
+var Size = require('famous-components').Size;
+var Mesh = require('famous-webgl-renderables').Mesh;
+var Geometry = require('famous-webgl-geometries').Plane;
+var Famous = require('famous-core').Famous;
+var Clock = Famous.getClock();
+
+/**
+ * Plane view constructing a basic WebGL mesh
+ */
+function Plane(node) {
+    this.dispatch = node.getDispatch();
+    this.position = new Position(this.dispatch);
+    this.mesh = new Mesh(this.dispatch);
+
+    /**
+     * Set the geometry to any of the given primitives: e.g. we have the Icosahedron required in above
+     * Set its color with (rgb, hsl, hex, color, hsv):
+     * Example: setColor('red'), setColor('#ff0000'), setColor(255, 0, 0)
+     *          setColor('hsl', 0, 100, 50), setColor('hex', '#ff0000'), setColor('rgb', 255, 0, 0), etc.
+     */
+    this.mesh.setGeometry(new Geometry());
+    this.mesh.setBaseColor('#232216');
+    this.mesh.setFlatShading(true);
+
+    this.position.setZ(-800);
+}
+
+
+/**
+ * Expose
+ */
+module.exports = Plane;
--- src/worker.js
+++ src/worker.js
@@ -7,7 +7,7 @@
 // module.exports = require('./examples/eventing/index');
 // module.exports = require('./examples/twitter/index');
 // module.exports = require('./examples/origin/index');
-module.exports = require('./examples/LightsGL/index');
-// module.exports = require('./examples/solarSystem/index');
+// module.exports = require('./examples/LightsGL/index');
+module.exports = require('./examples/solarSystem/index');
 // module.exports = require('./examples/noapi/index');
 // module.exports = require('./examples/noapi/layout');
