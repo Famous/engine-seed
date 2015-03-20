5097ebf7680beb91210abd442e7f3c9499630546
Mike O'Brien <michael.obrien.a@gmail.com>
2015-03-20 16:08:45 -0700

    chore: clean seed

--- src/worker.js
+++ src/worker.js
@@ -1,13 +1,7 @@
 // module.exports = require('./examples/gestures-demo/index');
 // module.exports = require('./examples/textureSharing/index');
-// module.exports = require('./examples/helloWorld/index');
-// module.exports = require('./examples/MATERIAL/index');
-// module.exports = require('./examples/basicLayout/index');
 // module.exports = require('./examples/objExample/index');
-// module.exports = require('./examples/eventing/index');
-// module.exports = require('./examples/twitter/index');
-// module.exports = require('./examples/origin/index');
-module.exports = require('./examples/LightsGL/index');
+// module.exports = require('./examples/LightsGL/index');
 // module.exports = require('./examples/solarSystem/index');
 // module.exports = require('./examples/noapi/index');
-// module.exports = require('./examples/physics-scroll/index');
+module.exports = require('./examples/physics-scroll/index');
