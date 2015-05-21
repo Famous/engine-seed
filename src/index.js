b2eb7669cd413a3909c7d3c624e7d69f8d679edf
Farhad Ghayour <f@famo.us>
2015-05-20 17:47:11 -0700

    Clean: Update spinner using components

--- src/index.js
+++ src/index.js
@@ -5,60 +5,33 @@ var DOMElement = require('famous/dom-renderables/DOMElement');
 var FamousEngine = require('famous/core/FamousEngine');
 
 // Boilerplate code to make your life easier
-
 FamousEngine.init();
 
-// Initialize the app by creating a scene
+// Initialize with a scene; add a 'node' to the scene root
+var logo = FamousEngine.createScene().addChild();
 
-var scene = FamousEngine.createScene();
+// Create an [image] DOM element providing the logo 'node' with a 'src'
+new DOMElement(logo, { tagName: 'img' })
+    .setAttribute('src', './images/famous_logo.png');
 
-// Add a 'node' element to the scene root
+// Set size mode to 'absolute' to use absolute pixel values: (width 250px, height 250px)
+logo
+    .setSizeMode('absolute', 'absolute', 'absolute')
+    .setAbsoluteSize(250, 250)
+    // Center the 'node' to the parent (the screen, in this instance)
+    .setAlign(0.5, 0.5)
+    // Set the translational point to the center of the 'node'
+    .setMountPoint(0.5, 0.5)
+    // Set the rotational origin to the center of the 'node'
+    .setOrigin(0.5, 0.5);
 
-var logo = scene.addChild();
-
-// Create an [image] DOM element providing the logo 'node'
-
-var el = new DOMElement(logo, { tagName: 'img' });
-
-// Set the source to the Famous logo asset
-
-el.setAttribute('src', './images/famous_logo.png');
-
-// Set size mode to 'absolute' to set with absolute pixel values: (width 250px, height 250px)
-
-logo.setSizeMode('absolute', 'absolute', 'absolute');
-logo.setAbsoluteSize(250, 250);
-
-// Center the 'node' to the parent (the screen, in this instance)
-
-logo.setAlign(0.5, 0.5);
-
-// Set the translational point to the center of the 'node'
-
-logo.setMountPoint(0.5, 0.5);
-
-// Set the rotational origin to the center of the 'node'
-
-logo.setOrigin(0.5, 0.5);
-
-// An object with an 'onUpdate' method that is called by the 'FamousEngine' singleton
-
-var animation = {
-
-    // Spin the logo on the Y-Axis
-
-    spinLogo: function() {
-        logo.setRotation(0, Date.now() * 0.0007, 0);
-    },
-
-    // Call 'spinLogo', every frame, by passing the 'animation' object back to the 'FamousEngine'
-
-    onUpdate: function() {
-        this.spinLogo();
-        FamousEngine.requestUpdateOnNextTick(animation);
+// Add a spinner component to the logo 'node' that is called, every frame
+var spinner = logo.addComponent({
+    onUpdate: function(time) {
+        logo.setRotation(0, time / 1000, 0);
+        logo.requestUpdateOnNextTick(spinner);
     }
-};
+});
 
-// And the magic happens...
-
-animation.onUpdate();
+// Let the magic begin...
+logo.requestUpdate(spinner);
