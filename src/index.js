a55cf1ce0fc24d908686703b1889bca140c70646
Farhad Ghayour <f@famo.us>
2015-05-20 17:31:36 -0700

    Feat: Update with the spinning logo and basic walk-through comments [#95105124]

--- src/index.js
+++ src/index.js
@@ -1,13 +1,64 @@
 'use strict';
 
+// Famous dependencies
 var DOMElement = require('famous/dom-renderables/DOMElement');
 var FamousEngine = require('famous/core/FamousEngine');
 
-// Boilerplate
+// Boilerplate code to make your life easier
+
 FamousEngine.init();
 
-// App Code
+// Initialize the app by creating a scene
+
 var scene = FamousEngine.createScene();
-var root = scene.addChild();
-var el = new DOMElement(root);
-el.setProperty('background', 'yellow');
+
+// Add a 'node' element to the scene root
+
+var logo = scene.addChild();
+
+// Create an [image] DOM element providing the logo 'node'
+
+var el = new DOMElement(logo, { tagName: 'img' });
+
+// Set the source to the Famous logo asset
+
+el.setAttribute('src', './images/famous_logo.png');
+
+// Set size mode to 'absolute' to set with absolute pixel values: (width 250px, height 250px)
+
+logo.setSizeMode('absolute', 'absolute', 'absolute');
+logo.setAbsoluteSize(250, 250);
+
+// Center the 'node' to the parent (the screen, in this instance)
+
+logo.setAlign(0.5, 0.5);
+
+// Set the translational point to the center of the 'node'
+
+logo.setMountPoint(0.5, 0.5);
+
+// Set the rotational origin to the center of the 'node'
+
+logo.setOrigin(0.5, 0.5);
+
+// An object with an 'onUpdate' method that is called by the 'FamousEngine' singleton
+
+var animation = {
+
+    // Spin the logo on the Y-Axis
+
+    spinLogo: function() {
+        logo.setRotation(0, Date.now() * 0.0007, 0);
+    },
+
+    // Call 'spinLogo', every frame, by passing the 'animation' object back to the 'FamousEngine'
+
+    onUpdate: function() {
+        this.spinLogo();
+        FamousEngine.requestUpdateOnNextTick(animation);
+    }
+};
+
+// And the magic happens...
+
+animation.onUpdate();
